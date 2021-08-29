14b186d1f297298f4ecb20b34d1e6beba45cf0e2
dkapper01 <dkapper01@gmail.com>
2021-08-28 19:05:19 -0500

    add projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,34 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'MicroUnicorn.io',
+    description: `Use these datasets to identify and research your competitors. Discover profitable businesses in niches you didn't know existed. All the while, you can stay up to date with the latest trends in the creator's economy.`,
+    imgSrc: '/static/images/micro-screenshot.png',
+    href: 'https://microunicorn.io/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    title: 'Jamstack24.com',
+    description: `I automated my freelance work for designers. This site helps me narrow the focus of the type of work I take on. It also establishes fixed pricing upfront.`,
+    imgSrc: '/static/images/jamstack24.png',
     href: '/blog/the-time-machine',
+  },
+  {
+    title: 'kodluyoruz.org',
+    description: `Kodluyoruz works to make Turkey a worldwide talent hub in the tech sector by creating the right pathways for youth.`,
+    imgSrc: '/static/images/kodluyoruz.png',
+    href: 'https://www.kodluyoruz.org/',
+  },
+  {
+    title: 'jaminary.com',
+    description: `Jaminary is a web development agency that focuses on building websites using Jamstack architecture.`,
+    imgSrc: '/static/images/jaminary.png',
+    href: 'https://www.jaminary.com/',
+  },
+  {
+    title: 'MediModo',
+    description: `A podcast I was on for a startup I helped co-found called MediModo.`,
+    imgSrc: '/static/images/podcast.jpg',
+    href:
+      'https://entrepreneuriallythinking.com/2017/07/26/4-4-local-entrepreneurs-riding-the-health-iot-wave/',
   },
 ]
 
