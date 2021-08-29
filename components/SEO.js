afc4f51f179e1d706119f296b08e2ffffaa6634b
dkapper01 <dkapper01@gmail.com>
2021-08-29 11:29:33 -0500

    move splibess script

--- components/SEO.js
+++ components/SEO.js
@@ -24,6 +24,7 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage }) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={twImage} />
+      <script async src="https://cdn.splitbee.io/sb.js"></script>
     </Head>
   )
 }
@@ -138,7 +139,6 @@ export const BlogSEO = ({ authorDetails, title, summary, date, lastmod, url, ima
         {date && <meta property="article:published_time" content={publishedAt} />}
         {lastmod && <meta property="article:modified_time" content={modifiedAt} />}
         <link rel="canonical" href={`${siteMetadata.siteUrl}${router.asPath}`} />
-        <script async src="https://cdn.splitbee.io/sb.js"></script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
