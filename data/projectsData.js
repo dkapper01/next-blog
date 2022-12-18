41cba89a5a339fb0590f6be23a78e3cd7bedb4fe
Daniel Kapper <danielkapper@Daniels-MacBook-Pro.local>
2022-12-18 13:34:10 -0500

    update project list data

--- data/projectsData.js
+++ data/projectsData.js
@@ -11,25 +11,25 @@ const projectsData = [
     imgSrc: '/static/images/jamstack24.png',
     href: 'https://www.jamstack24.com/',
   },
-  {
-    title: 'Kodluyoruz.org',
-    description: `Kodluyoruz works to make Turkey a worldwide talent hub in the tech sector by creating the right pathways for youth.`,
-    imgSrc: '/static/images/kodluyoruz.png',
-    href: 'https://www.kodluyoruz.org/',
-  },
-  {
-    title: 'Jaminary.com',
-    description: `Jaminary is a web development agency that focuses on building websites using Jamstack architecture.`,
-    imgSrc: '/static/images/jaminary.png',
-    href: 'https://www.jaminary.com/',
-  },
-  {
-    title: 'MediModo',
-    description: `A podcast I was on for a startup I helped co-found called MediModo.`,
-    imgSrc: '/static/images/podcast.jpg',
-    href:
-      'https://entrepreneuriallythinking.com/2017/07/26/4-4-local-entrepreneurs-riding-the-health-iot-wave/',
-  },
+  // {
+  //   title: 'Kodluyoruz.org',
+  //   description: `Kodluyoruz works to make Turkey a worldwide talent hub in the tech sector by creating the right pathways for youth.`,
+  //   imgSrc: '/static/images/kodluyoruz.png',
+  //   href: 'https://www.kodluyoruz.org/',
+  // },
+  // {
+  //   title: 'Jaminary.com',
+  //   description: `Jaminary is a web development agency that focuses on building websites using Jamstack architecture.`,
+  //   imgSrc: '/static/images/jaminary.png',
+  //   href: 'https://www.jaminary.com/',
+  // },
+  // {
+  //   title: 'MediModo',
+  //   description: `A podcast I was on for a startup I helped co-found called MediModo.`,
+  //   imgSrc: '/static/images/podcast.jpg',
+  //   href:
+  //     'https://entrepreneuriallythinking.com/2017/07/26/4-4-local-entrepreneurs-riding-the-health-iot-wave/',
+  // },
 ]
 
 export default projectsData
